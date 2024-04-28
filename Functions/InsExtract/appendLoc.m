b4d146ef1a19c2e1523df5dec4f13902df66b06e
c175044 <109503077+c175044@users.noreply.github.com>
2024-04-28 20:26:06 +0800

    resubmit

--- Functions/InsExtract/appendLoc.m
+++
@@ -1,9 +0,0 @@
-function LocP = appendLoc(img,Loc)
-%APPENDLOC 
-% drowZbarh(img,1);
-upimg = img(Loc(2):end,:);% drowZbarh(upimg,1);
-[~,LocInd] = maxk(drowZbarh(upimg,1,'Dwid'),2);
-LocP = [min(LocInd)+Loc(2)-1,max(LocInd)+Loc(2)-1;Loc];
-
-end
-
